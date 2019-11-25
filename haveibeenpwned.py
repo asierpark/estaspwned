684b2112d3b0bbfe7a11374b501057e3cd58056b
Asier Gomez <asier@trekkingdigital.com>
2019-11-25 16:58:29 +0100

    Comentarios traducidos al inglés

--- haveibeenpwned.py
+++ haveibeenpwned.py
@@ -1,5 +1,5 @@
 
-# Based on the python script of Laurens Houben
+# Based on the python script by Laurens Houben
 #   Example response for blup@blup.com:
 #   [{"Title":"Adobe","Name":"Adobe","Domain":"adobe.com","BreachDate":"2013-10-04","AddedDate":"2013-12-04T00:00:00Z","PwnCount":152445165,"Description":"In October 2013, 153 million Adobe accounts were breached with each containing an internal ID, username, email, <em>encrypted</em> password and a password hint in plain text. The password cryptography was poorly done and <a href=\"http://stricture-group.com/files/adobe-top100.txt\" target=\"_blank\" rel=\"noopener\">many were quickly resolved back to plain text</a>. The unencrypted hints also <a href=\"http://www.troyhunt.com/2013/11/adobe-credentials-and-serious.html\" target=\"_blank\" rel=\"noopener\">disclosed much about the passwords</a> adding further to the risk that hundreds of millions of Adobe customers already faced.","DataClasses":["Email addresses","Password hints","Passwords","Usernames"],"IsVerified":true,"IsSensitive":false,"IsActive":true,"IsRetired":false,"IsSpamList":false,"LogoType":"svg"},{"Title":"Xbox-Scene","Name":"Xbox-Scene","Domain":"xboxscene.com","BreachDate":"2015-02-01","AddedDate":"2016-02-07T20:26:56Z","PwnCount":432552,"Description":"In approximately February 2015, the Xbox forum known as <a href=\"http://xboxscene.com/\" target=\"_blank\" rel=\"noopener\">Xbox-Scene</a> was hacked and more than 432k accounts were exposed. The IP.Board forum included IP addresses and passwords stored as salted hashes using a weak implementation enabling many to be rapidly cracked.","DataClasses":["Email addresses","IP addresses","Passwords","Usernames"],"IsVerified":true,"IsSensitive":false,"IsActive":true,"IsRetired":false,"IsSpamList":false,"LogoType":"png"}]
 # -
@@ -47,7 +47,7 @@ def main():
 def checkAddress(email):
     sleep = rate # Reset default acceptable rate
     # API request here
-    # Now the API must be paid, unlucky but at least we have the script, add the key if you want paiddude
+    # Now the API must be paid, unlucky but at least we have the script, add the key if you want, paiddude
     check = requests.get("https://haveibeenpwned.com/api/v3/" + email + "/" +"?includeUnverified=true",
                  proxies = proxies,
                  verify = sslVerify)
