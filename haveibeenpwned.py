a2690f2874b9bc97c4ea0e9d71757cee1c1c9107
Asier Gomez <asier@trekkingdigital.com>
2019-11-25 16:56:22 +0100

    Comentarios traducidos al inglés

--- haveibeenpwned.py
+++ haveibeenpwned.py
@@ -1,3 +1,5 @@
+
+# Based on the python script of Laurens Houben
 #   Example response for blup@blup.com:
 #   [{"Title":"Adobe","Name":"Adobe","Domain":"adobe.com","BreachDate":"2013-10-04","AddedDate":"2013-12-04T00:00:00Z","PwnCount":152445165,"Description":"In October 2013, 153 million Adobe accounts were breached with each containing an internal ID, username, email, <em>encrypted</em> password and a password hint in plain text. The password cryptography was poorly done and <a href=\"http://stricture-group.com/files/adobe-top100.txt\" target=\"_blank\" rel=\"noopener\">many were quickly resolved back to plain text</a>. The unencrypted hints also <a href=\"http://www.troyhunt.com/2013/11/adobe-credentials-and-serious.html\" target=\"_blank\" rel=\"noopener\">disclosed much about the passwords</a> adding further to the risk that hundreds of millions of Adobe customers already faced.","DataClasses":["Email addresses","Password hints","Passwords","Usernames"],"IsVerified":true,"IsSensitive":false,"IsActive":true,"IsRetired":false,"IsSpamList":false,"LogoType":"svg"},{"Title":"Xbox-Scene","Name":"Xbox-Scene","Domain":"xboxscene.com","BreachDate":"2015-02-01","AddedDate":"2016-02-07T20:26:56Z","PwnCount":432552,"Description":"In approximately February 2015, the Xbox forum known as <a href=\"http://xboxscene.com/\" target=\"_blank\" rel=\"noopener\">Xbox-Scene</a> was hacked and more than 432k accounts were exposed. The IP.Board forum included IP addresses and passwords stored as salted hashes using a weak implementation enabling many to be rapidly cracked.","DataClasses":["Email addresses","IP addresses","Passwords","Usernames"],"IsVerified":true,"IsSensitive":false,"IsActive":true,"IsRetired":false,"IsSpamList":false,"LogoType":"png"}]
 # -
@@ -44,30 +46,30 @@ def main():
 
 def checkAddress(email):
     sleep = rate # Reset default acceptable rate
-    # AQUI SE CARGA LA API
-    # ahora la API es de pago por lo que nos devuelve siempre un 404. ucraniano cabrón -.-
+    # API request here
+    # Now the API must be paid, unlucky but at least we have the script, add the key if you want paiddude
     check = requests.get("https://haveibeenpwned.com/api/v3/" + email + "/" +"?includeUnverified=true",
                  proxies = proxies,
                  verify = sslVerify)
-    if str(check.status_code) == "404": # The address has not been breached.
+    if str(check.status_code) == "404":
         print check.status_code
         print OKGREEN + "[i] " + email + " has not been breached." + ENDC
-        time.sleep(sleep) # sleep so that we don't trigger the rate limit
+        time.sleep(sleep)
         return False
-    elif str(check.status_code) == "200": # The address has been breached!
+    elif str(check.status_code) == "200":
         print FAILRED + "[!] " + email + " has been breached!" + ENDC
-        time.sleep(sleep) # sleep so that we don't trigger the rate limit
+        time.sleep(sleep)
         return True
-    elif str(check.status_code) == "429": # Rate limit triggered
+    elif str(check.status_code) == "429":
         print WARNING + "[!] Rate limit exceeded, server instructed us to retry after " + check.headers['Retry-After'] + " seconds" + ENDC
         print WARNING + "    Refer to acceptable use of API: https://haveibeenpwned.com/API/v2#AcceptableUse" + ENDC
-        sleep = float(check.headers['Retry-After']) # Read rate limit from HTTP response headers and set local sleep rate
-        time.sleep(sleep) # sleeping a little longer as the server instructed us to do
-        checkAddress(email) # try again
+        sleep = float(check.headers['Retry-After'])
+        time.sleep(sleep)
+        checkAddress(email)
     else:
         print str(check.status_code)
         print WARNING + "[!] Something went wrong while checking " + email + ENDC
-        time.sleep(sleep) # sleep so that we don't trigger the rate limit
+        time.sleep(sleep)
         return True
 
 if __name__ == "__main__":
